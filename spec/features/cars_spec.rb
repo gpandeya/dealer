97b23d70f91380af82e857819cb271a28c25f6fd
Gyanesh Pandeya <gyanesh.pandeya@gmail.com>
2017-08-23 21:31:17 +0000

    show page

--- spec/features/cars_spec.rb
+++ spec/features/cars_spec.rb
@@ -4,10 +4,24 @@ RSpec.feature "Cars", type: :feature do
     scenario 'User views the /cars page' do
         car =Car.first
         visit '/cars'
+       
         expect(page).to have_text('Cars')
         expect(page).to have_text('Toyota')
         expect(page.html).to include('<h1>Cars</h1>')
         expect(page).to have_link('New',href: '/cars/new')
         expect(page).to have_link(car.vin,"/cars/#{car.id}")
     end
+    
+    scenario 'User views car show page' do
+        car = Car.first
+        visit '/cars'
+        click_link car.vin
+        
+        expect(page).to have_text(car.vin)
+        expect(page).to have_text(car.make)
+        expect(page).to have_link('Edit',href: "/cars/#{car.id}/edit")
+        expect(page).to have_link('Delete',href: "/cars/#{car.id}")
+        
+    end
+    
 end
