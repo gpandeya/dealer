2efddd183be9b4fed41c2559b179a937698a21da
Gyanesh Pandeya <gyanesh.pandeya@gmail.com>
2017-08-23 17:33:36 +0000

    few tests added

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,24 @@
+# == Schema Information
+#
+# Table name: cars
+#
+#  id           :integer          not null, primary key
+#  make         :string           not null
+#  model        :string           not null
+#  year         :integer          not null
+#  vin          :string           not null
+#  color        :string           default("black")
+#  category     :string           default("car")
+#  cylinders    :integer          default(4)
+#  displacement :float            default(0.0)
+#  mpg          :integer          default(0)
+#  hp           :integer          default(0)
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Car < ApplicationRecord
+    validates :make, :model, :year, :vin, presence: true
+    validates :vin, length: {is: 17}
+    
 end
--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: cars
+#
+#  id           :integer          not null, primary key
+#  make         :string           not null
+#  model        :string           not null
+#  year         :integer          not null
+#  vin          :string           not null
+#  color        :string           default("black")
+#  category     :string           default("car")
+#  cylinders    :integer          default(4)
+#  displacement :float            default(0.0)
+#  mpg          :integer          default(0)
+#  hp           :integer          default(0)
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Car, type: :model do
@@ -8,7 +27,49 @@ RSpec.describe Car, type: :model do
         expect(c.is_a?(Car)).to be true
         expect(c.attributes.keys.count).to eql(13)
     end
+ end
  
+ describe '#save' do
+     context 'happy path' do
+        it 'saves a car' do
+            c= Car.new(make: 'Ford', model: 'Fusion', year: 2015, vin: '0123456789abcdefg')
+            c.save
+            expect(c.id).to_not be_nil
+            expect(c.created_at).to_not be_nil
+            expect(c.updated_at).to_not be_nil
+        end
+    end
+    
+    context 'invalid data' do
+        it 'missing model - will not save' do
+            c= Car.new(make: 'Ford', year: 2015, vin: 'abcd')
+            c.save
+            expect(c.id).to be_nil
+            expect(c.errors[:model].first).to eql("can't be blank")
+            
+        end
+        
+        it 'missing model year - will not save' do
+            c= Car.new(make: 'Ford', vin: 'abcd')
+            c.save
+            expect(c.id).to be_nil
+            expect(c.errors[:model].first).to eql("can't be blank")
+            expect(c.errors[:year].first).to eql("can't be blank")
+            
+        end
+        
+         it 'vin number is too short - will not save' do
+            c= Car.new(make: 'Ford',model: 'Fusion', year: 2015, vin: 'abcd')
+            c.save
+            expect(c.id).to be_nil
+            expect(c.errors[:vin].first).to eql("is the wrong length (should be 17 characters)")
+           
+            
+        end
+    end
+    
+   
+    
  end
 
 
