d9c167aaf8eef7175d46226fbbeeb3f5c489c24d
Gyanesh Pandeya <gyanesh.pandeya@gmail.com>
2017-08-23 23:29:37 +0000

    controller test

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -8,4 +8,56 @@ class CarsController <ApplicationController
         @car = Car.find(params[:id])
     end
     
+    def destroy
+       
+        car = Car.find(params[:id])
+        car.destroy
+       
+        redirect_to cars_path
+
+    end
+    
+    def new
+        @car = Car.new
+    end
+    
+    def create
+        @car = Car.new(params[:car].permit(:vin, :make, :model, :year))
+       
+        if  @car.save
+            redirect_to car_path(@car)
+        else
+           
+            render 'new'
+        end
+    
+    end
+    
+    def edit
+        @car = Car.find(params[:id])
+    
+    end
+    
+    def update
+        @car = Car.find(params[:id])
+        
+        @car.vin = params[:car].permit([:vin])
+        @car.make = params[:car].permit([:make])
+        @car.model = params[:car].permit([:model])
+        @car.year = params[:car].permit([:year])
+        
+       
+        
+        if  @car.update
+            redirect_to car_path(@car)
+        else
+           
+            render 'edit'
+        end
+        
+        
+    end
+    
+    
+    
 end
--- spec/features/cars_spec.rb
+++ spec/features/cars_spec.rb
@@ -24,4 +24,67 @@ RSpec.feature "Cars", type: :feature do
         
     end
     
+     scenario 'User deletes a car' do
+        car = Car.first
+        visit "/cars/#{car.id}"
+        click_link 'Delete'
+        
+        expect(page).to have_text('Cars')
+        expect(page).to_not have_text(car.vin)
+
+        
+    end
+    
+    scenario 'User creates a new car' do
+      
+        visit "/cars/new"
+       
+        expect(page).to have_text('Add New Car')
+        fill_in 'car[vin]' , with: 'C0000000000000001'
+        fill_in 'car[make]' , with: 'AMC'
+        fill_in 'car[model]' , with: 'Gremlin'
+        fill_in 'car[year]' , with: '1973'
+        click_button 'Create Car'
+        
+        expect(page).to have_text('C0000000000000001')
+        expect(page).to have_text('AMC')
+        expect(page).to have_text('Gremlin')
+
+     
+    end
+    
+    scenario 'User creates a car with invalid VIN' do
+      
+        visit "/cars/new"
+       
+        expect(page).to have_text('Add New Car')
+        fill_in 'car[vin]' , with: 'C000000000000001'
+        fill_in 'car[make]' , with: 'AMC'
+        fill_in 'car[model]' , with: 'Gremlin'
+        fill_in 'car[year]' , with: '1973'
+        click_button 'Create Car'
+        
+        expect(page).to have_text('Add New Car')
+        expect(page).to have_text('is the wrong length (should be 17 characters')
+      
+     
+    end
+    
+     scenario 'User tries to edit car detail' do
+        car = Car.first
+        
+        visit "/cars//#{car.id}/edit"
+       
+        expect(page).to have_text('Edit Car')
+         fill_in 'car[vin]' , with: car.vin
+        fill_in 'car[make]' , with: 'AMC'
+        fill_in 'car[model]' , with: 'Gremlin'
+        fill_in 'car[year]' , with: '1973'
+        
+        click_button 'Edit Car'
+        
+        
+    end
+    
+    
 end
