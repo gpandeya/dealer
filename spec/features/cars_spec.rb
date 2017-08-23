5b2d1bc9d061ecbfb7d9f3dc612bd8137abca255
Gyanesh Pandeya <gyanesh.pandeya@gmail.com>
2017-08-23 21:04:43 +0000

    index page

--- spec/features/cars_spec.rb
+++ spec/features/cars_spec.rb
@@ -2,8 +2,12 @@ require 'rails_helper'
 
 RSpec.feature "Cars", type: :feature do
     scenario 'User views the /cars page' do
+        car =Car.first
         visit '/cars'
         expect(page).to have_text('Cars')
         expect(page).to have_text('Toyota')
+        expect(page.html).to include('<h1>Cars</h1>')
+        expect(page).to have_link('New',href: '/cars/new')
+        expect(page).to have_link(car.vin,"/cars/#{car.id}")
     end
 end
